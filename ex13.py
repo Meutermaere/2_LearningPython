bea2fb1b00c896a80f7cd8b7bf6a737dd46a1866
Supermarius <55379095+Supermarius@users.noreply.github.com>
2023-08-30 17:16:46 +0200

    Initial commit

---
+++ ex13.py
@@ -0,0 +1,6 @@
+from sys import argv
+script, first, second, third = argv
+print("Dieses Script heisst:", script)
+print("Die erste Variabel:", first)
+print("Die zweite Variabel:", second)
+print("Die dritte Variabel:", third)
